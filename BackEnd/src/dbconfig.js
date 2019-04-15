abe32f53bf5bb57601c6f16a3b5e2f9e1a68ee8b
Alejandro <alepiedra115@hotmail.com>
2019-04-15 14:17:05 -0600

    variables estáticas

--- BackEnd/src/dbconfig.js
+++ BackEnd/src/dbconfig.js
@@ -1,12 +1,12 @@
 module.exports = {
-  user: process.env.API_ENDPOINT,
+  user: 'alepie',
 
   // Instead of hard coding the password, consider prompting for it,
   // passing it in an environment variable via process.env, or using
   // External Authentication.
-  password: process.env.API_ENDPOINT,
+  password: 'Alajuela09',
 
   // For information on connection strings see:
   // https://oracle.github.io/node-oracledb/doc/api.html#connectionstrings
-  connectString: process.env.db_connection_string,
+  connectString: '172.30.82.20/ENODO',
 };
--- FrontEnd/src/app/utils/constants.ts
+++ FrontEnd/src/app/utils/constants.ts
@@ -1,10 +1,7 @@
 import {environment} from '../../environments/environment';
 
 export class Constants {
-  /*public static API_ENDPOINT='http://127.0.0.1:3000';
+  public static API_ENDPOINT='http://127.0.0.1:3000';
   public static USER='pfcti';
-  public static PASSWORD=123;*/
-  public static API_ENDPOINT=environment.API_ENDPOINT;
-  public static USER=environment.USER;
-  public static PASSWORD=environment.PASSWORD;
+  public static PASSWORD=123;
 }
